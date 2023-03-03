1678c86b11b1b920ed878779dfe348c3b0efba7f
Thomas Card <tcardlab@gmail.com>
2023-03-03 10:09:42 -0500

    Docs: todos

--- pyrallel/__init__.py
+++ pyrallel/__init__.py
@@ -1,3 +1,36 @@
 def joke():
-    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
-            u'Beiherhund das Oder die Flipperwaldt gersput.')
+  return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+    u'Beiherhund das Oder die Flipperwaldt gersput.')
+
+
+def uploadHelper(path, data):
+  # save to file? (or zip from memory)
+  # zip
+  # upload (pydrive)
+
+
+def completionChecker(path, partitions):
+  # are all files in path
+
+def importHelper():
+  # import all partitions
+
+def compilationHelper():
+  # importHelper
+  # compile
+  # uploadHelper
+
+def delHelper():
+  # delete all partitions, leave whole
+
+
+def pyrallel(path, array, function, partitions, postCompile=False, finDel=False):
+  # array is array? 
+  # ask which partition number to run (int)
+  # slice
+
+  # outArr = run function on arr (expect arr return?) # might want to handle other outputs in the future
+  # upload helper
+
+  # completion checker
+     # deletion checker
